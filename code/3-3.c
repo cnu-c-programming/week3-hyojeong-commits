a62b818f606bb43ca8639ba729dc19d548ad3901
hyojeong-commits <kweunsungo@gmail.com>
2026-03-26 21:38:41 +0900

    Update 3-3.c

--- code/3-3.c
+++ code/3-3.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main() {
+int average(int count, ...){
+    va_list ac;
+    va_start(ac, count);
+    int sum = ;
+    for(int i = 0; i < count; i++)
+        sum += va_arg(ac, int);
+    va_end(ap);
+    return sum/count;
+}
 
+int main() {
+    printf("%d\n", average(3, 10, 20, 30)); 
+    printf("%d\n", average(5, 10, 20, 30, 40, 50));
     return 0;
 }
