908e0595f96588e9f2055510448d5d313e726700
hyojeong-commits <kweunsungo@gmail.com>
2026-03-26 20:43:00 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+int power(int base, int exp){
+if (exp==0){return 1;}
+else { return base * power( base, exp-1);}
+}
 int main() {
+    printf("%d\n", power(2, 3));  // 8
+    printf("%d\n", power(5, 0));  // 1
+    printf("%d\n", power(3, 4));  // 81
 
     return 0;
 }
