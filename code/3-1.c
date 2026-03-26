6f5249e5db92f09be0887f9775a0cc3e790319b7
hyojeong-commits <kweunsungo@gmail.com>
2026-03-26 21:22:40 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+int call_out() {
+    static int a = 1; 
+    return ++count;
+}
 int main() {
+    printf("%d\n", call_count());
+    printf("%d\n", call_count());
+    printf("%d\n", call_count());
 
     return 0;
 }
